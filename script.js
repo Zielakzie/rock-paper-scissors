1136d3f218da8f554910ca878868f7a719142f47
Przemek <przemek.zielinski01@gmail.com>
2021-09-10 10:20:54 +0200

    timeout, no choice warrning

--- script.js
+++ script.js
@@ -1,32 +1,36 @@
-const array = ['Rock','Paper','Scissors']
+const array = ['Rock', 'Paper', 'Scissors']
 let playerChoise, computerChoice, gameResult = ""
 
 function computerTurn() {
-    const n = Math.floor(Math.random()* array.length)
+    const n = Math.floor(Math.random() * array.length)
     computerChoice = array[n]
 }
 
 function playerTurn() {
     playerChoise = document.getElementById('playerChoice').value
     document.getElementById('result').innerHTML = ''
-    
-
 }
 
 
 
-function game(){
+function game() {
     document.getElementById('result').innerHTML = ''
     playerTurn();
     computerTurn();
 
-    if (playerChoise === computerChoice){
-        gameResult = 'draw... try again?'
-    } else if (playerChoise === 'Paper' && computerChoice === 'Rock' || playerChoise === 'Rock' && computerChoice === 'Scissors' || playerChoise === 'Scissors' && computerChoice === 'Paper'){
-        gameResult = 'You win! GG :)'
-    } else if (playerChoise === 'Rock' && computerChoice === 'Paper' || playerChoise === 'Scissors' && computerChoice === 'Rock' || playerChoise === 'Paper' && computerChoice === 'Scissors'){
-        gameResult = 'You lose, one more round?'
-    }
+    setTimeout(function () {
+        if (playerChoise === computerChoice) {
+            gameResult = 'draw... try again?'
+        } else if (playerChoise === 'Paper' && computerChoice === 'Rock' || playerChoise === 'Rock' && computerChoice === 'Scissors' || playerChoise === 'Scissors' && computerChoice === 'Paper') {
+            gameResult = 'You win! GG :)'
+        } else if (playerChoise === 'Rock' && computerChoice === 'Paper' || playerChoise === 'Scissors' && computerChoice === 'Rock' || playerChoise === 'Paper' && computerChoice === 'Scissors') {
+            gameResult = 'You lose, one more round?'
+        } else {
+            gameResult = "It's seems like you are trying to cheat ;) Chose your weapon!"
+        }
 
-    document.getElementById('result').innerHTML = `Computer choice ${computerChoice}<br /><strong>${gameResult}</strong>`
+        document.getElementById('result').innerHTML = `Computer choice ${computerChoice}<br /><strong>${gameResult}</strong>`
+    }, 100);
+
+
 }
