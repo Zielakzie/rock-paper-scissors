32bd06e15fab62705b10e13ab08f4c25449bc54a
Przemek <przemek.zielinski01@gmail.com>
2021-09-10 08:16:49 +0200

    push

---
+++ script.js
@@ -0,0 +1,31 @@
+const array = ['Rock','Paper','Scissors']
+let playerChoise, computerChoice, gameResult = ""
+
+function computerTurn() {
+    const n = Math.floor(Math.random()* array.length)
+    computerChoice = array[n]
+}
+
+function playerTurn() {
+    playerChoise = document.getElementById('playerChoice').value
+    document.getElementById('result').innerHTML = ''
+    
+
+}
+
+
+
+function game(){
+    playerTurn();
+    computerTurn();
+
+    if (playerChoise === computerChoice){
+        gameResult = 'draw... try again?'
+    } else if (playerChoise === 'Paper' && computerChoice === 'Rock' || playerChoise === 'Rock' && computerChoice === 'Scissors' || playerChoise === 'Scissors' && computerChoice === 'Paper'){
+        gameResult = 'You win! GG :)'
+    } else if (playerChoise === 'Rock' && computerChoice === 'Paper' || playerChoise === 'Scissors' && computerChoice === 'Rock' || playerChoise === 'Paper' && computerChoice === 'Scissors'){
+        gameResult = 'You lose, one more round?'
+    }
+
+    document.getElementById('result').innerHTML = `Computer choice ${computerChoice}<br /><strong>${gameResult}</strong>`
+}
